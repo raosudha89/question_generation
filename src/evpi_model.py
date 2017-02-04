56824c51c9f2b8f0c394e2ebd68ce526b1f1658e
Sudha Rao <raosudha@umiacs.umd.edu>
2017-02-04 07:40:12 -0500

    Working EVPI model

--- src/evpi_model.py
+++ src/evpi_model.py
@@ -7,7 +7,7 @@ import theano.tensor as T
 from collections import Counter
 import pdb
 import time
-import random
+import random, math
 
 def get_data_masks(content, max_len):
 	if len(content) > max_len:
@@ -161,21 +161,24 @@ def build_lstm(word_embeddings, len_voc, word_emb_dim, N, args, freeze=False):
 	post_out = T.mean(post_out, axis=0)
 	
 	pred_ans_out = [None]*N
-	#post_pred_ans_out = [None]*N
+	pqa_out = [None]*(N*N)
 	for i in range(N):
-		pred_ans_out[i] = T.sum(T.stack([post_out, ques_out[i]], axis=2), axis=2)
-		#post_pred_ans_out[i] = T.sum(T.stack([post_out, pred_ans_out[i]], axis=2), axis=2)
-	
-	#errors = [None]*N
-	#for i in range(N):
-	#	errors[i] = T.sum(lasagne.objectives.squared_error(pred_ans_out[i], ans_out[i]), axis=1)
-
-	#loss = T.sum(lasagne.objectives.squared_error(pred_ans_out[0], ans_out[0]))
+		for j in range(N):
+			if i == j:
+				pred_ans_out[i] = T.sum(T.stack([post_out, ques_out[i]], axis=2), axis=2)
+			pqa_out[i*10+j] = T.sum(T.stack([post_out, ques_out[i], ans_out[j]], axis=2), axis=2)
 	
 	errors = [None]*N
 	for i in range(N):
 		errors[i] = lasagne.objectives.squared_error(pred_ans_out[i], ans_out[i])
+	#for i in range(1, N):
+	#	loss += T.sum(abs(lasagne.objectives.squared_error(pred_answer_out[0], answer_out[i]) - lasagne.objectives.squared_error(question_out[0], question_out[i])))
 	loss = T.sum(errors * T.transpose(labels)[:,:,None])
+
+	all_sq_errors = [None]*(N*N)
+	for i in range(N):
+		for j in range(N):
+			all_sq_errors[i*10+j] = T.sum(lasagne.objectives.squared_error(pred_ans_out[i], ans_out[j]), axis=1)
 
 	utility_post_sents = T.itensor3()
 	utility_post_sent_masks = T.ftensor3()
@@ -226,17 +229,17 @@ def build_lstm(word_embeddings, len_voc, word_emb_dim, N, args, freeze=False):
 	utility_preds = lasagne.layers.get_output(l_utility_post_dense)
 	utility_loss = T.sum(lasagne.objectives.binary_crossentropy(utility_preds, utility_labels))
 	
-	l_pred_ans_out = [None]*N
-	l_pred_ans_dense = [None]*N
-	utility_pred_ans = [None]*N
+	l_pqa_out = [None]*(N*N)
+	l_pqa_dense = [None]*(N*N)
+	utility_pqa = [None]*(N*N)
 	for i in range(N):
-		l_pred_ans_out[i] = lasagne.layers.InputLayer(shape=(None, args.hidden_dim), input_var=pred_ans_out[i])
-		#l_pred_ans_out[i] = lasagne.layers.InputLayer(shape=(None, args.hidden_dim), input_var=post_pred_ans_out[i])
-		l_pred_ans_dense[i] = lasagne.layers.DenseLayer(l_pred_ans_out[i], num_units=1,\
+		for j in range(N):
+			l_pqa_out[i*10+j] = lasagne.layers.InputLayer(shape=(None, args.hidden_dim), input_var=pqa_out[i*10+j])
+			l_pqa_dense[i*10+j] = lasagne.layers.DenseLayer(l_pqa_out[i*10+j], num_units=1,\
                                                     	nonlinearity=lasagne.nonlinearities.sigmoid,\
 														W=l_utility_post_dense.W,\
 														b=l_utility_post_dense.b)
-		utility_pred_ans[i] = lasagne.layers.get_output(l_pred_ans_dense[i])
+			utility_pqa[i*10+j] = lasagne.layers.get_output(l_pqa_dense[i*10+j])
 
 	post_params = lasagne.layers.get_all_params(l_post_lstm[0], trainable=True)
 	post_emb_params = lasagne.layers.get_all_params(l_post_emb[0], trainbale=True)
@@ -257,9 +260,9 @@ def build_lstm(word_embeddings, len_voc, word_emb_dim, N, args, freeze=False):
 	utility_updates = lasagne.updates.adam(utility_loss, utility_all_params, learning_rate=args.learning_rate)
 
 	train_fn = theano.function([post_sents, post_sent_masks, ques_list, ques_masks_list, ans_list, ans_masks_list, labels], \
-									[loss] + utility_pred_ans, updates=updates)
+									[loss] + utility_pqa + all_sq_errors, updates=updates)
 	dev_fn = theano.function([post_sents, post_sent_masks, ques_list, ques_masks_list, ans_list, ans_masks_list, labels], \
-									[loss] + utility_pred_ans,)
+									[loss] + utility_pqa + all_sq_errors,)
 	utility_train_fn = theano.function([utility_post_sents, utility_post_sent_masks, utility_labels], \
 									[utility_preds, utility_loss], updates=utility_updates)
 	utility_dev_fn = theano.function([utility_post_sents, utility_post_sent_masks, utility_labels], \
@@ -300,20 +303,24 @@ def utility_iterate_minibatches(post_sents, post_sent_masks, labels, batch_size,
 		data.append([post_sents[excerpt], post_sent_masks[excerpt], labels[excerpt]])
 	return data
 
-#def get_test_pred(utilities, errors)
-#	marginalized_utilities = [0.0]*len(utilities)
-#	for i in range(utilities):
-#		marginalized_utilities[i] = sum([])
+def get_rank(utilities, labels):
+	pdb.set_trace()
+	correct = np.where(labels==1)[0][0]
+	sort_index_utilities = np.argsort(utilities)
+	rank = np.where(sort_index_utilities==correct)[0][0]
+	return len(utilities) - rank #since ascending sort and we want descending
 
 def validate(val_fn, utility_val_fn, fold_name, epoch, fold, utility_fold, args):
 	start = time.time()
 	num_batches = 0
 	cost = 0
 	corr = 0
+	mrr = 0
 	total = 0
 	utility_corr = 0
 	utility_total = 0
 	utility_cost = 0
+	_lambda = 0.5
 	N = args.no_of_candidates
 	N_post_sents = args.post_max_sents
 	post_sents, post_sent_masks, ques_list, ques_masks_list, ans_list, ans_masks_list = fold
@@ -335,13 +342,24 @@ def validate(val_fn, utility_val_fn, fold_name, epoch, fold, utility_fold, args)
 		am = np.transpose(am, (1, 0, 2))
 		out = val_fn(p, pm, q, qm, a, am, l)
 		loss = out[0]
-		#errors = out[1]
-		preds = out[1:]
+		preds = out[1:N*N+1]
 		preds = np.transpose(preds, (1, 0, 2))
+		errors = out[N*N+1:]
+		errors = np.transpose(errors, (1, 0))
+		#for j in range(len(preds)):
+		#	if np.argmax(preds[j*10+j]) == np.argmax(l[j]):
+		#		corr += 1
+		#	total += 1
 		for j in range(len(preds)):
-			if np.argmax(preds[j]) == np.argmax(l[j]):
+			utilities = [0.0]*N
+			for k in range(N):
+				for m in range(N):
+					utilities[k] += math.exp(-_lambda * errors[j][k*10+m])*preds[j][k*10+m]
+			if np.argmax(utilities) == np.argmax(l[j]):
 				corr += 1
+			mrr += 1.0/(1+get_rank(utilities, l[j]))
 			total += 1
+		
 		cost += loss
 		up, upm, ul = utility_minibatches[i]
 		up = np.transpose(up, (1, 0, 2))	
@@ -353,9 +371,9 @@ def validate(val_fn, utility_val_fn, fold_name, epoch, fold, utility_fold, args)
 			utility_total += 1
 		utility_cost += utility_loss
 
-	lstring = '%s: epoch:%d, cost:%f, utility_cost:%f, acc:%f, utility_acc:%f time:%d' % \
+	lstring = '%s: epoch:%d, cost:%f, utility_cost:%f, acc:%f, mrr:%f, utility_acc:%f time:%d' % \
 				(fold_name, epoch, cost*1.0/num_batches, utility_cost*1.0/num_batches, \
-					corr*1.0/total, utility_corr*1.0/utility_total, time.time()-start)
+					corr*1.0/total, mrr*1.0/total, utility_corr*1.0/utility_total, time.time()-start)
 	print lstring
 
 def main(args):
