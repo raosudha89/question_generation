8a1a8995e2612b9f0772dd6bf42388f3a5390941
Sudha Rao <raosudha@umiacs.umd.edu>
2017-10-12 15:53:25 -0400

    Deleted stale old files

--- scripts/combine_plain_texts.sh
+++
@@ -1,7 +0,0 @@
-DATADIR=/fs/clip-scratch/raosudha/datasets/stackoverflow
-SCRIPTDIR=/fs/clip-amr/question_generation/scripts
-for site in $DATADIR/*;
-do
-	echo $site
-	cat $site/plain_text.txt >> $DATADIR/stackexchange_datadump.txt
-done
